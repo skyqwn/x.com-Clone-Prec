0d1e2083f0f746e6f89d39438f4fe5ad430951e7
kimba <skyqwn@gmail.com>
2023-12-12 17:48:12 +0900

    first commit

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,15 @@
+import styles from "@/app/page.module.css";
+
+type Props = {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+};
+
+export default function Layout({ children, modal }: Props) {
+  return (
+    <div className={styles.container}>
+      {children}
+      {modal}
+    </div>
+  );
+}
---
+++ src/app/home/layout.tsx
@@ -0,0 +1,7 @@
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>홈레이아웃{children}</div>;
+}
